9b7db154e697e8fbcf2e015dd29bb7d50e2dea36
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2024-01-26 15:28:51 +0100

    english comments

--- river-monitoring-service/river_monitoring_service.py
+++ river-monitoring-service/river_monitoring_service.py
@@ -1,3 +1,7 @@
+# Author: Annibalini Lorenzo
+# Author: Bacchini Lorenzo
+# Author: Sanchi Emanuele
+
 import time
 import signal
 import threading
@@ -7,7 +11,7 @@ import serial.tools.list_ports
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qs
 
-#Enum per stati del sistema
+#Enum for machine states
 class State(Enum):
     NORMAL = 0, "Normal"
     ALARM_TOO_LOW = 1, "Alarm too low"
@@ -15,26 +19,24 @@ class State(Enum):
     ALARM_TOO_HIGH = 3, "Alarm too high"
     ALARM_TOO_HIGH_CRITIC = 4, "Alarm too high critic"
     
-#Costanti per valori frequenza
+#Frequency constants in milliseconds
 F1 = 1000
 F2 = 500
 
-#Costanti per valori soglia
+# Water level constants
 WL1 = 0.1
 WL2 = 0.15
 WL3 = 0.3
 WL4 = 0.35
 
-# Impostazioni del broker MQTT
+# MQTT settings
 broker_address = "172.20.10.5"
 server_port = 1883
 topic_send = "frequency"
 topic_receive = "water-level"
 
-#impostazioni server HTTP
 
-
-# Variabili globali
+# Global variables
 frequency_message = 0
 valve_value = 0
 manual_valve_value = 0
@@ -43,7 +45,7 @@ state = State.NORMAL
 http_received = ""
 manual = False
 
-# Classe che gestisce le richieste HTTP
+# Class for HTTP server
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         global state, water_level, valve_value, manual_valve_value, manual
@@ -56,9 +58,7 @@ class MyHandler(BaseHTTPRequestHandler):
                 self.send_header('Content-type','text-html')
                 self.end_headers()
 
-                # Invia una risposta al client
-                # get the state of the system with the string value of the enum
-                
+                #send message back to client               
                 message = "state:" + str(state.value[1]) + ";water_level:" + str(water_level) + ";valve_value:" + str(manual_valve_value if manual else valve_value)
                 self.wfile.write(message.encode())
                 return
@@ -72,6 +72,7 @@ class MyHandler(BaseHTTPRequestHandler):
             self.send_header('Content-type','text-html')
             self.end_headers()
 
+            #set state to manual and get valve value
             s = self.path.split("?")[1]
             value = parse_qs(s)["value"][0]
             if(not manual):
@@ -79,11 +80,12 @@ class MyHandler(BaseHTTPRequestHandler):
                 manual = True
             else:
                 manual = False
-                
+            
+            #send message back to client  
             self.wfile.write("ok".encode())
             return
 
-# Funzione per avviare il server HTTP
+# Function to run HTTP server
 def run():
     server_address = ('localhost', 8080)
     httpd = HTTPServer(server_address, MyHandler)
@@ -91,7 +93,7 @@ def run():
     
     httpd.serve_forever()
     
-    # Aggiungi la gestione del segnale di terminazione
+    # Handler per il segnale di terminazione
     def handler(signum, frame):
         print("Ricevuto segnale di terminazione. Chiudo il server.")
         httpd.socket.close()
@@ -100,23 +102,22 @@ def run():
 
 
 
-# Stampa delle porte seriali disponibili
+# Print serial ports available
 print("Porte seriali disponibili:")
 ports = serial.tools.list_ports.comports()
 for port, desc, hwid in sorted(ports):
     print(f"{port}: {desc} [{hwid}]")
 
-# Input utente per la porta seriale
+# Ask user to select a serial port
 selected_port = input("Inserisci il nome della porta seriale che vuoi utilizzare: ")
 
-# Connessione alla porta seriale
-#ser = serial.Serial(selected_port, 9600, timeout=1)
+# Connect to serial port
 ser = serial.Serial()
 ser.baudrate = 9600
 ser.port = selected_port
 ser.open()
 
-# Callback che gestisce la connessione al broker
+# what to do on connecting to broker
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         client.subscribe(topic_receive)
@@ -124,35 +125,36 @@ def on_connect(client, userdata, flags, rc):
     else:
         print(f"Errore di connessione al broker, codice: {rc}")
 
-# Funzione per inviare un messaggio al topic "frequency"
+# send message to broker
 def publish_message(client, message):
     client.publish(topic_send, message)
     
-# Callback che gestisce la ricezione di un messaggio
+# Callback to receive message from broker
 def on_message(client, userdata, msg):
     global water_level
     water_level = float(msg.payload.decode())
     print(f"Ricevuto messaggio: {water_level}")
 
-# Creazione del client MQTT
+# MQTT client creation
 client = mqtt.Client()
 
-# Connessione al broker
+# broker connection
 client.connect(broker_address, server_port, 60)
 client.subscribe(topic_receive)
 client.on_message = on_message  # Set the on_message callback
 client.loop_start()
 
-# Avvio del server HTTP
+# Start HTTP server in a separate thread
 http_thread = threading.Thread(target=run)
 http_thread.start()
 
-
+# Sleep for 2 seconds to let the HTTP server start
 time.sleep(2)
 
-# Loop principale
+# Main loop
 try:
     while True:
+        # Check water level and set state
         if(water_level < WL1):
             frequency_message = F1
             state = State.ALARM_TOO_LOW
@@ -174,19 +176,20 @@ try:
             state = State.ALARM_TOO_HIGH_CRITIC
             valve_value = 100
             
-        # Invia un messaggio con la frequenza desiderata all'esp
+        # Send message to broker with frequency value for ESP
         publish_message(client, frequency_message)
         
-        # Invia messaggio ad Arduino
+        # Send message to Arduino with valve value
         if(manual) :
             msg = (str(manual_valve_value)).encode()
         else :
             msg = (str(valve_value)).encode()
         ser.write(msg)
 
-        # Attendi prima di inviare il prossimo messaggio
+        # Sleep for frequency value
         time.sleep(frequency_message/1000)
 
+# Interrupt main loop on CTRL+C
 except KeyboardInterrupt:
     print("Interruzione del loop infinito")
     client.disconnect()
